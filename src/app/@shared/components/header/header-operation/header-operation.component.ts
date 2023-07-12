5c3d2588574de1df5d24dbd4c86a0f516540babc
haidv <dinhvanhai159@gmail.com>
2023-07-12 17:34:06 +0700

    haidv upcode

--- src/app/@shared/components/header/header-operation/header-operation.component.ts
+++ src/app/@shared/components/header/header-operation/header-operation.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, ViewChildren } from '@angular/core';
 import { Router } from '@angular/router';
+import { DialogService } from 'ng-devui/modal';
+import { ApiService } from 'src/app/api.service';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'src/app/@core/services/auth.service';
 import { LANGUAGES } from 'src/config/language-config';
 import { User } from '../../../models/user';
+import { FormLayout, DValidateRules } from 'ng-devui';
 import { I18nService } from 'ng-devui/i18n';
 import { Users } from 'src/app/@core/data/listData';
 
@@ -13,13 +16,41 @@ import { Users } from 'src/app/@core/data/listData';
   styleUrls: ['./header-operation.component.scss'],
 })
 export class HeaderOperationComponent implements OnInit {
+  @ViewChild('changePass', { static: true }) changePass: TemplateRef<any>;
   user: Users;
   languages = LANGUAGES;
   language: string;
   haveLoggedIn = false;
   noticeCount: number;
+  msgs: Array<Object> = [];
+  formData = {
+    newPass: '',
+    oldPass: '',
+    renewPass: '',
+  };
+  formRules: { [key: string]: DValidateRules } = {
+    oldPasswordRules: {
+      // validators: [{ required: true }, { minlength: 6 }, { maxlength: 15 }, { pattern: /^[a-zA-Z0-9\d@$!%*?&.]+(\s+[a-zA-Z0-9]+)*$/ }],
+      validators: [{ required: true , message: "Không để trống dòng này!" }],
+      message: 'Đã có lỗi khi nhập.',
+    },
+    passwordRules: {
+      // validators: [{ required: true }, { minlength: 6 }, { maxlength: 15 }, { pattern: /^[a-zA-Z0-9\d@$!%*?&.]+(\s+[a-zA-Z0-9]+)*$/ }],
+      validators: [{ required: true , message: "Không để trống dòng này!" }, { minlength: 6, message: "Mật khẩu phải từ 6 kí tự trở lên!" }, { maxlength: 15, message: "Mật khẩu không quá 15 kí tự!" },],
+      message: 'Nhập dữ liệu không đúng định dạng.',
+    },
+  };
+  showPassword = false;
+  currentInput = 'newPass';
+  horizontalLayout: FormLayout = FormLayout.Horizontal;
+  newUser  = {
+    user_rcd: "",
+    pass_word:"",
+    new_password:"",
+  };
+  
 
-  constructor(private route: Router, private authService: AuthService, private translate: TranslateService, private i18n: I18nService) {}
+  constructor(private route: Router, private authService: AuthService, private translate: TranslateService, private i18n: I18nService, private dialogService: DialogService, private api: ApiService) {}
 
   ngOnInit(): void {
     if (localStorage.getItem('userinfo')) {
@@ -37,7 +68,7 @@ export class HeaderOperationComponent implements OnInit {
   }
 
   onSearch(event: any) {
-    console.log(event);
+    // console.log(event);
   }
 
   onLanguageClick(language: string) {
@@ -62,5 +93,52 @@ export class HeaderOperationComponent implements OnInit {
 
   handleNoticeCount(event: number) {
     this.noticeCount = event;
+  }
+  openModalChangePass() {
+    this.dialogService.open({
+      id: 'edit-dialog',
+      width: '40%',
+      title: 'Đổi mật khẩu',
+      showAnimate: false,
+      contentTemplate: this.changePass,
+      backdropCloseable: true,
+      onClose: () => {
+        this.resetInputPass();
+      },
+      buttons: [],
+    });
+  }
+  saveChangePass({ valid, directive, data, errors }: any) {
+    if(valid) {
+      if(this.formData.newPass == this.formData.oldPass) {
+        this.showToast('error',"Mật khẩu mới không trùng mật khẩu cũ!");
+        return;
+      }
+      if(this.formData.newPass != this.formData.renewPass) {
+        this.showToast('error',"Mật khẩu mới không trùng khớp!");
+        return;
+      }
+      this.newUser.user_rcd = this.user.user_rcd;
+      this.newUser.pass_word = this.formData.oldPass;
+      this.newUser.new_password = this.formData.newPass;
+      this.api.post("api/manager/UserRef/ChangePass/", this.newUser).subscribe((res:any) => {
+        this.showToast('success',res.data);
+        console.log(res);
+      })
+    }
+    else{
+
+    }
+  }
+  resetInputPass() {
+    this.formData = {
+      newPass: '',
+      oldPass: '',
+      renewPass: '',
+    };
+    this.showPassword = false;
+  }
+  showToast(type:any, message: any) {
+    this.msgs = [{ severity: type, summary: 'Thông báo', content: message }];
   }
 }
